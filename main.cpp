887ee2c0197fac4f4db1fca87bdde79b238e8184
Allen <allen.brd.75@gmail.com>
2025-03-10 14:38:55 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <memory>
 #include <sstream>
+#include <iomanip>
 
 // Include the httplib header
 // #define CPPHTTPLIB_OPENSSL_SUPPORT
@@ -171,6 +172,29 @@ public:
         return response;
     }
 
+    std::string escape_json(const std::string &s) {
+        std::ostringstream o;
+        for (auto c = s.cbegin(); c != s.cend(); c++) {
+            switch (*c) {
+            case '"': o << "\\\""; break;
+            case '\\': o << "\\\\"; break;
+            case '\b': o << "\\b"; break;
+            case '\f': o << "\\f"; break;
+            case '\n': o << "\\n"; break;
+            case '\r': o << "\\r"; break;
+            case '\t': o << "\\t"; break;
+            default:
+                if ('\x00' <= *c && *c <= '\x1f') {
+                    o << "\\u"
+                      << std::hex << std::setw(4) << std::setfill('0') << (int)*c;
+                } else {
+                    o << *c;
+                }
+            }
+        }
+        return o.str();
+    }
+
     void run() {
         httplib::Server server;
 
@@ -228,7 +252,7 @@ public:
             
             // Create JSON response
             std::ostringstream json_response;
-            json_response << "{\"response\":\"" << response << "\"}";
+            json_response << "{\"response\":\"" << escape_json(response) << "\"}";
             
             res.set_content(json_response.str(), "application/json");
         });
